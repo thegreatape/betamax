17466b032a119b73a5ba3d3b1d5c17e08126ba76
Thomas Mayfield <Thomas.Mayfield@gmail.com>
2013-11-30 17:56:36 -0500

    Test for refusing connection if target is down.

--- proxy/proxy_test.go
+++ proxy/proxy_test.go
@@ -1,6 +1,7 @@
 package proxy_test
 
 import (
+	"fmt"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	. "github.com/thegreatape/betamax/proxy"
@@ -15,11 +16,15 @@ import (
 var _ = Describe("Proxy", func() {
 	var proxy *httputil.ReverseProxy
 	var targetListener net.Listener
+	var targetPort string
 	var proxyListener net.Listener
+	var proxyPort string
 
 	BeforeEach(func() {
-		targetListener, _ = net.Listen("tcp", "0.0.0.0:8081")
-		proxyListener, _ = net.Listen("tcp", "0.0.0.0:8080")
+		targetListener, _ = net.Listen("tcp", "0.0.0.0:0")
+		_, targetPort, _ = net.SplitHostPort(targetListener.Addr().String())
+		proxyListener, _ = net.Listen("tcp", "0.0.0.0:0")
+		_, proxyPort, _ = net.SplitHostPort(targetListener.Addr().String())
 
 		go http.Serve(targetListener, http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
 			io.WriteString(writer, "hello, world")
@@ -27,6 +32,7 @@ var _ = Describe("Proxy", func() {
 
 		targetUrl, _ := url.Parse("http://127.0.0.1:8081/")
 		proxy = Proxy(targetUrl)
+		go http.Serve(proxyListener, proxy)
 	})
 
 	AfterEach(func() {
@@ -35,12 +41,17 @@ var _ = Describe("Proxy", func() {
 	})
 
 	It("proxies without any configuration", func() {
-		go http.Serve(proxyListener, proxy)
-
-		resp, _ := http.Get("http://127.0.0.1:8080")
+		resp, _ := http.Get(fmt.Sprintf("http://127.0.0.1:%s", proxyPort))
 		defer resp.Body.Close()
 
 		body, _ := ioutil.ReadAll(resp.Body)
 		Expect(string(body)).To(Equal("hello, world"))
+	})
+
+	It("refuses conneciton if the target server is down", func() {
+		targetListener.Close()
+		_, err := http.Get(fmt.Sprintf("http://127.0.0.1:%s", proxyPort))
+		Expect(err).ToNot(BeNil())
+		Expect(err.Error()).To(ContainSubstring("connection refused"))
 	})
 })
