c3c146c474528e362acd73393cbcb0af8a05b75d
Thomas Mayfield <Thomas.Mayfield@gmail.com>
2013-11-30 19:00:25 -0500

    Fix previous test setup and add GET for current config.

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -1,10 +1,22 @@
 package proxy
 
 import (
+	"fmt"
+	"net/http"
 	"net/http/httputil"
 	"net/url"
 )
 
-func Proxy(target *url.URL) *httputil.ReverseProxy {
-	return httputil.NewSingleHostReverseProxy(target)
+func handleConfig(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
+		if req.Method == "GET" && req.URL.Path == "/__betamax__/config" {
+			fmt.Fprintf(resp, "{\"cassette\": null}")
+		} else {
+			handler.ServeHTTP(resp, req)
+		}
+	})
+}
+
+func Proxy(target *url.URL) http.Handler {
+	return handleConfig(httputil.NewSingleHostReverseProxy(target))
 }
--- proxy/proxy_test.go
+++ proxy/proxy_test.go
@@ -1,6 +1,7 @@
 package proxy_test
 
 import (
+	"encoding/json"
 	"fmt"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -9,12 +10,11 @@ import (
 	"io/ioutil"
 	"net"
 	"net/http"
-	"net/http/httputil"
 	"net/url"
 )
 
 var _ = Describe("Proxy", func() {
-	var proxy *httputil.ReverseProxy
+	var proxy http.Handler
 	var targetListener net.Listener
 	var targetPort string
 	var proxyListener net.Listener
@@ -24,13 +24,13 @@ var _ = Describe("Proxy", func() {
 		targetListener, _ = net.Listen("tcp", "0.0.0.0:0")
 		_, targetPort, _ = net.SplitHostPort(targetListener.Addr().String())
 		proxyListener, _ = net.Listen("tcp", "0.0.0.0:0")
-		_, proxyPort, _ = net.SplitHostPort(targetListener.Addr().String())
+		_, proxyPort, _ = net.SplitHostPort(proxyListener.Addr().String())
 
 		go http.Serve(targetListener, http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
 			io.WriteString(writer, "hello, world")
 		}))
 
-		targetUrl, _ := url.Parse("http://127.0.0.1:8081/")
+		targetUrl, _ := url.Parse(fmt.Sprintf("http://127.0.0.1:%s/", targetPort))
 		proxy = Proxy(targetUrl)
 		go http.Serve(proxyListener, proxy)
 	})
@@ -48,10 +48,25 @@ var _ = Describe("Proxy", func() {
 		Expect(string(body)).To(Equal("hello, world"))
 	})
 
-	It("refuses conneciton if the target server is down", func() {
+	It("returns a 500 if the target server is down", func() {
 		targetListener.Close()
-		_, err := http.Get(fmt.Sprintf("http://127.0.0.1:%s", proxyPort))
-		Expect(err).ToNot(BeNil())
-		Expect(err.Error()).To(ContainSubstring("connection refused"))
+		resp, err := http.Get(fmt.Sprintf("http://127.0.0.1:%s", proxyPort))
+		Expect(err).To(BeNil())
+		Expect(resp.StatusCode).To(Equal(500))
+	})
+
+	Context("allows configuration over http", func() {
+		It("returns the current configuration with a GET to /__betamax__/config", func() {
+			resp, err := http.Get(fmt.Sprintf("http://127.0.0.1:%s/__betamax__/config", proxyPort))
+			Expect(err).To(BeNil())
+
+			var jsonResponse interface{}
+			body, _ := ioutil.ReadAll(resp.Body)
+			err = json.Unmarshal(body, &jsonResponse)
+			Expect(err).To(BeNil())
+
+			jsonData := jsonResponse.(map[string]interface{})
+			Expect(jsonData["cassette"]).To(BeNil())
+		})
 	})
 })
